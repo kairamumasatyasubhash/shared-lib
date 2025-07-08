92f91ac06ac5ec6dcbfae151334d2373968ed64f
kairamumasatyasubhash <skairam@softility.com>
2025-07-08 12:18:57 +0530

    addwed shered-lib

---
+++ src/com/subhash/builds/calculator.groovy
@@ -0,0 +1,24 @@
+//methods 
+package com.subhash.builds;
+
+//define class
+class calculator {
+    def jenkins
+
+
+    calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+
+
+    def methodname(arg1, arg2) {
+        return logicalcode
+    }
+    def add(firstnumber, secoundnumber) {
+        retun firstnumber=secoundnumber
+  }
+
+  def multiply(firstNumber, secoundNumber) {
+    return firstn
+  }
+}
